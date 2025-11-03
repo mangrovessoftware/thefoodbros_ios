377f3c7d5ef8a851ad80be0ef6db7f5baa261f7b
MuthuGanapathy <muthuganapathy.dev@gmail.com>
2025-11-04 00:09:23 +0530

    Config EventCategory model

---
+++ FoodBros/Models/EventCategory.swift
@@ -0,0 +1,14 @@
+//
+//  EventCategory.swift
+//  FoodBros
+//
+//  Created by Muthu on 04/11/25.
+//
+
+import Foundation
+
+struct EventCategory: Identifiable {
+    let id = UUID()
+    let name: String
+    let iconName: String
+}
